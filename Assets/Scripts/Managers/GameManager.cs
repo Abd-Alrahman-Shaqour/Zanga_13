0a63316d5fba34479c33c25bc84b263970ae35a6
Abd Alrahman Shaqour <abdmaher01@gmail.com>
2025-07-10 21:22:21 +0300

    Main

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : SingletonPersistent<GameManager>
+{
+    public GameState state;
+    public static event Action<GameState> OnGameStateChanged;
+    // public PlayerCore PlayerCore { get; set;}
+
+    protected override void Awake()
+    {
+        base.Awake();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+    }
+    private void OnDestroy()
+    {
+        // Clean up event subscriptions
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
+    }
+
+    private void Start()
+    {
+        UpdateGameState(GameState.MainMenu);
+    }
+
+    public void UpdateGameState(GameState newState)
+    {
+        state = newState;
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                Time.timeScale = 1f;
+                break;
+            case GameState.Paused:
+                Time.timeScale = 0f;
+                break;
+            case GameState.GamePlay:
+                Time.timeScale = 1f;
+                break;
+            case GameState.GameOver:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+
+        OnGameStateChanged?.Invoke(newState);
+    }
+
+    public void LeveCompleted()
+    {
+
+    }
+
+    public void SaveGame()
+    {
+        // PlayerCore ??= FindObjectOfType<PlayerCore>();
+
+        //if (PlayerCore != null)
+        //_saveManager.SerializeJson(PlayerCore.playerStats);
+
+    }
+        public void PauseGame()
+    {
+        UpdateGameState(GameState.Paused);
+    }
+
+    public void ResumeGame()
+    {
+        UpdateGameState(GameState.GamePlay);
+    }
+    public void GameOver()
+    {
+        UpdateGameState(GameState.GameOver);
+    }
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log($"Scene loaded: {scene.name}");
+
+        // Handle scene-specific initialization
+        if (scene.name.Contains("Level_"))
+        {
+            UpdateGameState(GameState.GamePlay);
+        }
+        else if (scene.name == "MainMenu")
+        {
+            UpdateGameState(GameState.MainMenu);
+        }
+    }
+
+    private void OnSceneUnloaded(Scene scene)
+    {
+        Debug.Log($"Scene unloaded: {scene.name}");
+    }
+
+}
+
+public enum GameState
+{
+    MainMenu,
+    Paused,
+    GamePlay,
+    GameOver
+}
---
+++ Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject();
+                    singletonObject.name = typeof(T).Name;
+                    _instance = singletonObject.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_instance == this)
+            _instance = null;
+    }
+}
+public class SingletonPersistent<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+    
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject();
+                    singletonObject.name = typeof(T).Name;
+                    _instance = singletonObject.AddComponent<T>();
+                }
+                
+                // Make sure the singleton persists across scene loads
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+            
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            // If another instance already exists, destroy this one
+            Destroy(gameObject);
+        }
+    }
+}
+
