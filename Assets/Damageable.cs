13a729104382ef990d8ec37d7c4005033738c861
YousefTaha-Spoilz <youseftahadot1994@gmail.com>
2025-07-13 13:33:29 +0300

    Updates

---
+++ Assets/Damageable.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using StarterAssets;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    [Header("Main components")]
+    [SerializeField] ThirdPersonController_RobotSphere robotSphere_controller;
+
+    [Header("Main properties")]
+    [SerializeField] int health = 1;
+    [SerializeField] int shield;
+
+    [SerializeField]
+
+    void Awake()
+    {
+         
+    }
+
+    public void Init()
+    {
+        shield = robotSphere_controller.has_Shield;
+    }
+
+    public void TakeDamage()
+    {
+        if(shield > 0)
+        {
+            shield--;
+            return;
+        }
+
+        if(health > 0)
+        {
+            health--;
+
+            if(health <= 0)
+            {
+                // Death
+            }
+        }
+    }
+
+    public void PlayerDied()
+    {
+        robotSphere_controller.canPlay = false;
+    }
+}
